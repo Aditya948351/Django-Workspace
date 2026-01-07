741b93b2c05ecccc9441caa982ec74f039215375
Aditya948351 <adityapatil9721@gmail.com>
2026-01-07 15:06:23 +0530

    Added Image assets

--- FirstProject/FirstProject/urls.py
+++ FirstProject/FirstProject/urls.py
@@ -21,5 +21,6 @@ from FirstProject import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery')
 ]
