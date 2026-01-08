bc20c14631749821bb6fe8d19ab074d46c9ad807
Aditya948351 <adityapatil9721@gmail.com>
2026-01-08 15:19:58 +0530

    Add factorial calculator feature
    Added a new factorial calculator view and template. Updated URLs to include the factorial route. Enhanced the add view to process POST requests and display results. Improved the add.html template for better form handling and result display.

--- CalculatorProject/calculator/views.py
+++ CalculatorProject/calculator/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def add(request):
-    return render(request, 'calculator/add.html')
+    if request.method == "GET":
+        return render(request, 'calculator/add.html')
+    else:
+        fn = int(request.POST.get('num1'))
+        sn = int(request.POST.get('num2'))
+        res = fn + sn
+        data = { 
+            'firstnum': fn,
+            'secondnum': sn,
+            'result': res
+        }
+        return render(request, 'calculator/add.html', data)
+
+def factorial(request):
+    if request.method == "GET":
+        return render(request, 'calculator/factorial.html')
+    else:
+        num = int(request.POST.get('number'))
+        fact = 1
+        for i in range(1, num + 1):
+            fact = fact*i
+        data = {
+            'number': num,
+            'result': fact
+        }
+        return render(request, 'calculator/factorial.html', data)
