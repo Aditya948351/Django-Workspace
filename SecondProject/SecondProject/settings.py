60122200e7e3125ac39548238dc09d9ae2357372
Aditya948351 <adityapatil9721@gmail.com>
2026-01-09 11:31:03 +0530

    Configure static files for deployment
    Updated settings.py to set ALLOWED_HOSTS for deployment, added WhiteNoise middleware, and defined STATIC_ROOT for static file collection. Modified wsgi.py to expose 'app' for compatibility. Added collected static files for the Django admin and project assets to the staticfiles directory.

--- SecondProject/SecondProject/settings.py
+++ SecondProject/SecondProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r9d$gu_3i!t5omd^*6$dl@2eq&rfvp0o_bq7#8pb6_n2*7k5&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
