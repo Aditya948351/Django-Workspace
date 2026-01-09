8a78f968e90b6c6c707fd924e5130057fbb38c4d
Aditya948351 <adityapatil9721@gmail.com>
2026-01-09 12:25:40 +0530

    done

--- StatProject/statapp/urls.py
+++ StatProject/statapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('display/', views.display, name='display'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contact, name='contact')
 ]
--- StatProject/statapp/views.py
+++ StatProject/statapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def display(request):
+    return render(request, 'display.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
